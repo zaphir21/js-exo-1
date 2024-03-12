4a9ed42e511dadd5725bab922869c8b8ca23ad4c
zaphir21 <zaphir21>
2024-03-12 16:27:22 +0100

    Ajouter les statistiques et les types des Pokémon à l'affichage

--- pokedex/script.js
+++ pokedex/script.js
@@ -16,8 +16,32 @@ fetch(url)
         image.src = pokemon.image
         image.width = 100
         image.height = 100
+        const statsDev = document.createElement("div")
+        statsDev.className = "stats"
+
+        Object.keys(pokemon.stats).map((key) => {
+            const stat = document.createElement("p")
+            stat.textContent = `${key.toUpperCase()}: ${pokemon.stats[key]}`
+            statsDev.appendChild(stat)
+
+        })
+        const typeDiv = document.createElement("div")
+        typeDiv.className = "type"
+        pokemon.apiTypes.map(type => {
+            const typeP = document.createElement("p")
+            typeP.textContent = type.name
+            const typeImg = document.createElement("img")
+            typeImg.src = type.image
+            typeImg.width = 50
+            typeImg.height = 50
+            typeDiv.appendChild(typeP)
+            typeDiv.appendChild(typeImg)
+        })
+        pokemonDiv.appendChild(typeDiv)
         pokemonDiv.appendChild(image)
         pokemonDiv.appendChild(para)
+        pokemonDiv.appendChild(statsDev)
+
         divRootNode.appendChild(pokemonDiv)
     })
 })
